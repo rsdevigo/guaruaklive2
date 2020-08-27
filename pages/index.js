852a2ac5230c3da7491a707e44c764aa1a3c0d8b
rsdevigo <rsdevigo@gmail.com>
2020-08-27 17:12:14 -0400

    Primeiro commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,73 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styled from "@emotion/styled";
+import fetch from "isomorphic-unfetch";
+import Cabecalho from "../components/Cabecalho";
+import Hero from "../components/Hero";
+import Conteudo from "../components/Conteudo";
+import Equipe from "../components/Equipe";
+import Rodape from "../components/Rodape";
 
-export default function Home() {
+export default function Home(props) {
+  const HomeStyled = styled.header`
+    h1 {
+      color: red;
+      font-size: 10rem;
+    }
+
+    p {
+      color: green;
+      font-style: italic;
+    }
+  `;
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>{props.geral.nomeApp}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Cabecalho
+        redeSocial={props.geral.RedesSociais}
+        logo={props.geral.LogoApp}
+        menu={props.menu}
+        fundo={props.hero.fundo.url}
+      />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Hero hero={props.hero} />
+      <Conteudo conteudo={props.conteudo} />
+      <Equipe equipe={props.equipe} />
+      <Rodape rodape={props.rodape} />
     </div>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  const geralRes = await fetch("https://guaruak.herokuapp.com/geral");
+  const geral = await geralRes.json();
+
+  const geralMenu = await fetch("https://guaruak.herokuapp.com/menu");
+  const menu = await geralMenu.json();
+
+  const heroRes = await fetch("https://guaruak.herokuapp.com/hero");
+  const hero = await heroRes.json();
+
+  const conteudoRes = await fetch("https://guaruak.herokuapp.com/conteudo");
+  const conteudo = await conteudoRes.json();
+
+  const equipeRes = await fetch("https://guaruak.herokuapp.com/equipe");
+  const equipe = await equipeRes.json();
+
+  const rodapeRes = await fetch("https://guaruak.herokuapp.com/rodape");
+  const rodape = await rodapeRes.json();
+
+  return {
+    props: {
+      geral,
+      menu,
+      hero,
+      conteudo,
+      equipe,
+      rodape,
+    },
+  };
 }
